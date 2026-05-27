c2ca9e9c73bdbe8d6a5306c9ac2212cbf050c74f
rushi4240 <baterushi1213@gmail.com>
2026-05-27 16:20:03 +0530

    add validation to comapny model

--- FetchFake_API/Models/Company.cs
+++ FetchFake_API/Models/Company.cs
@@ -1,9 +1,13 @@
-﻿namespace FetchFake_API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FetchFake_API.Models
 {
     public class Company
     {
         public string name { get; set; }
+        [Required]
         public string catchPhrase { get; set; }
+        [Required]
         public string bs { get; set; }
     }
 }
