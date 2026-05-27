0e294cba5295b61682f5451404536316b6fbfb39
rushi4240 <baterushi1213@gmail.com>
2026-05-27 16:41:47 +0530

    change in user model

--- FetchFake_API/Models/User.cs
+++ FetchFake_API/Models/User.cs
@@ -1,15 +1,23 @@
-﻿namespace FetchFake_API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FetchFake_API.Models
 {
     public class User
     {
 
         public int id { get; set; }
         public string name { get; set; }
+        [Required]
         public string username { get; set; }
+        [Required]
         public string email { get; set; }
+        [Required]
         public Address address { get; set; }
+        [Required]
         public string phone { get; set; }
+        [Required]
         public string website { get; set; }
+        [Required]
         public Company company { get; set; }
     }
 
